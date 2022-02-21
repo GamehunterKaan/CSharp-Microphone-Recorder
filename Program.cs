757964c4c7a24e405cac6542b3a23f660767f344
GamehunterKaan <68380968+GamehunterKaan@users.noreply.github.com>
2022-02-21 20:59:50 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+using System.Runtime.InteropServices;
+
+
+namespace Microphone_Module
+{
+    public class Program
+    {
+        [DllImport("winmm.dll")]
+
+       public static extern long mciSendString(
+
+       string strCommand,
+
+       string strReturn,
+
+       int iReturnLength,
+
+       IntPtr oCallback,
+
+       int lpszDeviceID
+       );
+        static void Main(string[] args)
+        {
+            int sleep = (int.Parse(args[0])*1000);
+            mciSendString("open new Type waveaudio Alias recsound", "", 0, IntPtr.Zero, 1);
+            mciSendString("record recsound", null, 0, IntPtr.Zero, 1);
+            Thread.Sleep(sleep);
+            mciSendString("save recsound " + args[1], "", 0, IntPtr.Zero ,1);
+            mciSendString("close recsound ", null, 0, IntPtr.Zero, 1);
+        }
+    }
+}
